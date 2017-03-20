ba2bd8ec1b6aee6f47193b5d6023e91a70b82ddd
drewk <drewk@DESKTOP-5AU53C7>
2017-03-20 08:30:53 -0500

    Add project files.

---
+++ BootstrapIntroduction/Models/Person.cs
@@ -0,0 +1,8 @@
+﻿namespace BootstrapIntroduction.Controllers
+{
+    internal class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
