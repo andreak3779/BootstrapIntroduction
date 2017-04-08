eb49e0d81d6daad27389cf217e200a6705942b50
andreak <shimmeringbluorb@gmail.com>
2017-04-08 10:48:00 -0500

    Added connection string for code first EF

--- BootstrapIntroduction/DAL/BookContext.cs
+++ BootstrapIntroduction/DAL/BookContext.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BootstrapIntroduction.Models;
+using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Data.Entity.ModelConfiguration.Conventions;
