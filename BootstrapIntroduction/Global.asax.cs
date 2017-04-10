565fcf9d686519ef6baafe9641d27681cfff39b9
andreak <shimmeringbluorb@gmail.com>
2017-04-10 08:55:55 -0500

    chapter 5 changes for EF and removed Database First Code

--- BootstrapIntroduction/Global.asax.cs
+++ BootstrapIntroduction/Global.asax.cs
@@ -5,6 +5,8 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using BootstrapIntroduction.DAL;
+using System.Data.Entity;
 
 namespace BootstrapIntroduction
 {
@@ -16,6 +18,10 @@ namespace BootstrapIntroduction
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            var bookContext = new BookContext();
+            Database.SetInitializer(new BookInitializer());
+            bookContext.Database.Initialize(true);
         }
     }
 }
